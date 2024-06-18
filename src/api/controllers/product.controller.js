9dc62ab95a06730d7ffec19d7c6967063512f627
sunil kumar sahoo <sunilsahoosks2002@gmail.com>
2024-06-18 22:56:21 +0530

    Upadting product controller

--- src/api/controllers/product.controller.js
+++ src/api/controllers/product.controller.js
@@ -1,16 +1,19 @@
 import { Product } from "../../db/models/product.model.js";
+import { v4 as uuid } from "uuid";
 
-export const NewProduct = (req, res) => {
-  const { product_id, name, desc, price } = req.body;
+//funtion for newproduct creation
+export const NewProduct = async (req, res) => {
+  const { name, desc, price } = req.body;
   try {
-
-    if(!product_id || !name ||! desc || !price ){
-        return res.status(409).json({
-            messgae: "Please provide the required detail's",
-            success: false,
-          });
+    if (!name || !desc || !price) {
+      return res.status(409).json({
+        messgae: "Please provide the product detail's",
+        success: false,
+      });
     }
-    const newProduct = Product.create({
+    const id = uuid();
+    const product_id = `P${id}`.split("-").join("").substring(0, 10);
+    const newProduct = await Product.create({
       product_id,
       name,
       desc,
@@ -30,11 +33,113 @@ export const NewProduct = (req, res) => {
       newProduct,
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).json({
-        messgae: "error while creating new product",
+      messgae: "error while creating new product",
+      success: false,
+    });
+  }
+};
+
+//funtion for getting all product
+export const getAllProduct = async (req, res) => {
+  try {
+    const allProduct = await Product.findAll();
+
+    if (!allProduct || allProduct.length == 0) {
+      return res.status(404).json({
+        messgae: "No product found.",
         success: false,
-        
       });
+    }
+
+    return res.status(200).json({
+      messgae: "Getting all product",
+      success: true,
+      allProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      messgae: "error while fetching all product",
+      success: false,
+    });
+  }
+};
+
+//funtion for deleting exsting product
+export const updateProduct = async (req, res) => {
+  const { product_id, name, price, desc } = req.body;
+  try {
+    if (!product_id) {
+      return res.status(409).json({
+        message: "Please provide product id",
+        success: false,
+      });
+    }
+
+    const existProduct = await Product.findByPk(product_id);
+
+    if (!existProduct || existProduct.length == 0) {
+      return res.status(404).json({
+        messgae: "No product found.",
+        success: false,
+      });
+    }
+    if (name) {
+      existProduct.name = name;
+    }
+    if (price) {
+      existProduct.price = price;
+    }
+    if (desc) {
+      existProduct.desc = desc;
+    }
+    await existProduct.save();
+
+    return res.status(200).json({
+      messgae: "Product updated successfully",
+      success: true,
+      existProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      messgae: "error while updatinf=g the product",
+      success: false,
+    });
+  }
+};
+//funtion for updating exsting product
+export const deleteProduct = async (req, res) => {
+  const { product_id } = req.body;
+  try {
+    if (!product_id) {
+      return res.status(409).json({
+        message: "Please provide product id",
+        success: false,
+      });
+    }
+
+    const existProduct = await Product.findByPk(product_id);
+
+    if (!existProduct || existProduct.length == 0) {
+      return res.status(404).json({
+        messgae: "No product found.",
+        success: false,
+      });
+    }
+    await existProduct.destroy();
+
+    return res.status(200).json({
+      messgae: "Product deleted successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      messgae: "error while deleting the product",
+      success: false,
+    });
   }
 };
--- src/api/routes/product.route.js
+++ src/api/routes/product.route.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { NewProduct } from "../controllers/product.controller.js";
+import { NewProduct, deleteProduct, getAllProduct, updateProduct } from "../controllers/product.controller.js";
 
 export const productRoutes=express.Router();
 
 
 productRoutes.post("/new",NewProduct)
+productRoutes.get("/all",getAllProduct)
+productRoutes.put("/update",updateProduct)
+productRoutes.delete("/delete",deleteProduct)
+
+
+
 
